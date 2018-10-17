053a00f95b23b594606c5393471230ea57dabd5b
AlbDav <albano.1708530@studenti.uniroma1.it>
2018-10-17 19:33:23 +0200

    qr

--- routes/ticket.js
+++ routes/ticket.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var qr = require('qrcode');
 var rand_str = require('randomstring');
+var ejs = require('ejs');
+var pdf = require('html-pdf');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,8 +11,19 @@ router.get('/', function(req, res, next) {
 	var token = rand_str.generate();
 	var filename = '/tickets/' + token + '.png';
 	qr.toFile(path + filename,'Questo è il ticket con token ' + token, function(err){
-		console.log('done');
-		res.render('ticket', {img_path: filename});
+		console.log('img done');
+		ejs.renderFile(path + '/views/ticket.ejs', {img_path: filename}, function(err, result){
+			if(result){
+				pdf.create(result).toFile(path + '/tickets/' + token + '.pdf', function(error, resultpdf){
+					if(err){
+						console.log('errore');
+					}
+					else{
+						console.log(resultpdf);
+					}
+				}
+			}
+		});	
 	});
 });
 
