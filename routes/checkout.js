5044360eefd3fa1551e1887816ef728840957ed9
AlbDav <albano.1708530@studenti.uniroma1.it>
2018-10-20 21:16:42 +0200

    payment

--- routes/checkout.js
+++ routes/checkout.js
@@ -48,7 +48,7 @@ router.post('/', function(req, res, next) {
 					}
 					console.log(tr_result);
 					res.send(tr_result);
-				});*/
+				});
                         });*/
                 }
         });
