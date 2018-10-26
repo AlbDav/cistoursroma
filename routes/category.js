fb9773a47b31c8752686aded4376bead819d251f
AlbDav <albano.1708530@studenti.uniroma1.it>
2018-10-26 20:10:43 +0200

    index style

--- routes/category.js
+++ routes/category.js
@@ -15,7 +15,7 @@ router.get('/:id', function(req, res, next) {
 	var id = req.params.id;
 	
 	pool.query('SELECT * FROM categories WHERE category_id = $1', [id], (err, result) => {
-		var category = result.rows;
+		var category = result.rows[0];
 		pool.query('SELECT * FROM products, products_en, prices WHERE category = $1 AND products.product_id = products_en.product_id AND products.product_id = prices.product_id AND prices.option_num = 1 ORDER BY products.product_id', [id], (err, result) => {
 			var products = result.rows;
 			res.render('category', {category, products});
